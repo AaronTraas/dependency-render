6a72d4247fe462952ee52fc30a09f9c9330f5b72
Aaron Traas <aaron@traas.org>
2023-09-10 09:20:08 -0400

    Manually merged Ted's changes

--- dependency-render.py
+++ dependency-render.py
@@ -9,11 +9,10 @@ import os
 class ApplicationNode:
     def __init__(self, name, group, vendor, slo, dependencies):
         self.name = name
-        self.group = group
+        self.group = None if not group else group
         self.vendor = vendor
         self.slo = None if not slo else float(slo)
-        self.dependencies = [] if not dependencies else dependencies.split(',')
-
+        self.dependencies = set() if not dependencies else set(dependencies.split(','))
 
 class Config:
     # columns of CSV
@@ -49,7 +48,7 @@ def ingest_applications_from_csv(config):
     parse CSV file; ignores first row.
     '''
     applications = {}
-    with open(config.input_csv_path) as csvfile:
+    with open(config.input_csv_path, "rt", encoding="utf-8") as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader) # Skip header row
         for row in csvreader:
@@ -70,10 +69,12 @@ def dependency_set_to_graph(applications):
     '''
 
     dot = graphviz.Digraph(comment='Dependencies',
-        graph_attr={'rankdir':'LR'},
+        graph_attr={'rankdir':'RL'},
         node_attr={'color': '#dddddd', 'style': 'filled', 'fontcolor':'#777777'},
         edge_attr={'color': 'darkgrey'})
     
+    dot_endl = '<BR />'
+
     # create nodes that only exist as dependencies, but aren't defined as rows in the CSV.
     for app in applications.values():
         for dep_id in app.dependencies:
@@ -81,47 +82,51 @@ def dependency_set_to_graph(applications):
                 dot.node(dep_id, f'{dep_id}\n(undefined)', fillcolor='#eeeeee', color='#ff9999', style='dashed,filled')
 
     # create node and connections for each defined applications
+    all_deps = set.union(*[ app.dependencies for app in applications.values() ])
+    known_apps = set(applications.keys())
+    # undefined_deps = all_deps - known_apps
+    for dep_id in all_deps.difference(known_apps):
+        dot.node(dep_id, f'<{dep_id}{dot_endl}(undefined)>', fillcolor='#eeeeee',
+                 color='#ff9999', fontcolor=None, style='dashed,filled')
+
+    # create node and connections for each defined applications
     for app_id, app in applications.items():
-        slo_label = ''
         if app.slo:
-            app_slo = float(app.slo)*100
-            slo_label = f'\navailability: {app.slo}%'
+            slo_label   = f'{dot_endl}availability: {app.slo*100}%'
             nodecolor   = 'white'
             textcolor   = 'black'
             bordercolor = 'black'
         else:
+            slo_label   = '' #f'{dot_endl}availability: N/A'
             nodecolor   = None
             textcolor   = None
             bordercolor = None
 
-        vendor_label = ''
-        if app.vendor:
-            vendor_label = f'\n({app.vendor})'
+        vendor_label = '' if not app.vendor else f'{dot_endl}({app.vendor})'
 
-        dot.node(app_id, f'{app.name}{vendor_label}{slo_label}', group=app.group, color=bordercolor, fillcolor=nodecolor, fontcolor=textcolor)
+        group_name = app.group
+
+        dot.node(app_id, f'<{app.name}{vendor_label}{slo_label}>',
+                 group=group_name, color=bordercolor,
+                 fillcolor=nodecolor, fontcolor=textcolor)
 
         # create connections to dependencies
         for dep_id in app.dependencies:
-            line_color='darkgrey'
-            if dep_id in applications.keys():
+            line_color=None
+            if dep_id in known_apps:
                 dep_slo = applications.get(dep_id).slo
-                if dep_slo:
-                    dep_slo = float(dep_slo)*100
 
-                if (not app_slo) or (not dep_slo):
-                    line_color = None
-                elif app_slo <= dep_slo:
-                    line_color='green'
-                else:
-                    line_color='red'
-            dot.edge(app_id, dep_id, dir='back', constraint='true', color=line_color)
+                if (app.slo is not None) and (dep_slo is not None):
+                    line_color='green' if app.slo <= dep_slo else 'red'
+
+            dot.edge(dep_id, app_id, color=line_color)
 
     return dot
 
 
 def render_output(config):
     dot = dependency_set_to_graph(ingest_applications_from_csv(config))
-    dot.format=config.output_type;
+    dot.format=config.output_type
     dot.render(config.output_filename)
 
 
